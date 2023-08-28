13b23c09f57f56c6063d397c7c335d00b62283ff
sunkyuj <sunkyuj12@gmail.com>
2023-08-28 15:37:57 +0900

    init

---
+++ tarotdream/src/main/java/com/sunkyuj/tarotdream/TarotdreamApplication.java
@@ -0,0 +1,13 @@
+package com.sunkyuj.tarotdream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotdreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarotdreamApplication.class, args);
+	}
+
+}
---
+++ tarotdream/src/test/java/com/sunkyuj/tarotdream/TarotdreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunkyuj.tarotdream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarotdreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
